17896d8ceb539c8fba6a54765eb5df70d44129cc
Mustafa <mustafacs17@acropolis.in>
2021-04-16 18:20:36 +0530

    basic structure

--- screens/MainTabScreen.js
+++ screens/MainTabScreen.js
@@ -1,108 +1,98 @@
 import React from 'react';
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import Icon from 'react-native-vector-icons/Ionicons';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Icon} from 'react-native-elements';
 
 import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
-import ExploreScreen from './ExploreScreen';
 import ProfileScreen from './ProfileScreen';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const HomeStack = createStackNavigator();
-const DetailsStack = createStackNavigator();
-
 const Tab = createMaterialBottomTabNavigator();
 
 const MainTabScreen = () => (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor="#fff"
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeStackScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarColor: '#009387',
-          tabBarIcon: ({ color }) => (
-            <Icon name="ios-home" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={DetailsStackScreen}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarColor: '#1f65ff',
-          tabBarIcon: ({ color }) => (
-            <Icon name="ios-notifications" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarColor: '#694fad',
-          tabBarIcon: ({ color }) => (
-            <Icon name="ios-person" color={color} size={26} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Explore"
-        component={ExploreScreen}
-        options={{
-          tabBarLabel: 'Explore',
-          tabBarColor: '#d02860',
-          tabBarIcon: ({ color }) => (
-            <Icon name="ios-aperture" color={color} size={26} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+  <Tab.Navigator initialRouteName="Home" activeColor="#fff" shifting>
+    <Tab.Screen
+      name="Home"
+      component={HomeStackScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarColor: '#075E54',
+        tabBarIcon: ({color}) => (
+          <Icon name="home" type="antdesign" color={color} size={24} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileStackScreen}
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarColor: '#00A699',
+        tabBarIcon: ({color}) => (
+          <Icon name="account-circle" type="material" color={color} size={24} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
 );
 
 export default MainTabScreen;
 
+const renderLeft = navigation => {
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.openDrawer()}
+      style={{paddingLeft: 12}}>
+      <Icon name="menu" type="feather" color="white" size={28} />
+    </TouchableOpacity>
+  );
+};
+
 const HomeStackScreen = ({navigation}) => (
-<HomeStack.Navigator screenOptions={{
-        headerStyle: {
-        backgroundColor: '#009387',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-        fontWeight: 'bold'
-        }
+  <HomeStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#075E54',
+      },
+      headerTintColor: '#fff',
+      headerTitleAlign: 'center',
+      headerTitleStyle: {
+        fontWeight: '600',
+        fontSize: 20,
+      },
     }}>
-        <HomeStack.Screen name="Home" component={HomeScreen} options={{
-        title:'Overview',
-        headerLeft: () => (
-            <Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={() => navigation.openDrawer()}></Icon.Button>
-        )
-        }} />
-</HomeStack.Navigator>
+    <HomeStack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        title: 'Music-Con',
+        headerLeft: () => renderLeft(navigation),
+      }}
+    />
+  </HomeStack.Navigator>
 );
 
-const DetailsStackScreen = ({navigation}) => (
-<DetailsStack.Navigator screenOptions={{
-        headerStyle: {
-        backgroundColor: '#1f65ff',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-        fontWeight: 'bold'
-        }
+const ProfileStackScreen = ({navigation}) => (
+  <HomeStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#00A699',
+      },
+      headerTintColor: '#fff',
+      headerTitleAlign: 'center',
+      headerTitleStyle: {
+        fontWeight: '600',
+        fontSize: 20,
+      },
     }}>
-        <DetailsStack.Screen name="Details" component={DetailsScreen} options={{
-        headerLeft: () => (
-            <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
-        )
-        }} />
-</DetailsStack.Navigator>
+    <HomeStack.Screen
+      name="Profile"
+      component={ProfileScreen}
+      options={{
+        title: 'Profile',
+        headerLeft: () => renderLeft(navigation),
+      }}
+    />
+  </HomeStack.Navigator>
 );
-  
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, Text, Button, StatusBar, StyleSheet} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
 const ProfileScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Profile Screen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
-      </View>
-    );
+  const {colors} = useTheme();
+
+  const theme = useTheme();
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
+      <Text style={{color: colors.text}}>Profile Screen</Text>
+      <Button title="Click Here" onPress={() => alert('Button Clicked!')} />
+    </View>
+  );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
